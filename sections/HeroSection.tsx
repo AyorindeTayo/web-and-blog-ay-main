dc2637a5b8b44ee9492b4e66e08616ae97d8918f
AyorindeTayo <olanipekunayo2010@yahoo.com>
2023-06-11 06:16:02 +0200

    first 565

--- sections/HeroSection.tsx
+++ sections/HeroSection.tsx
@@ -138,7 +138,7 @@ const HeroSection: React.FC = () => {
             exceptional results.
           </p>
           <p>
-            With my expertise in building machine learning models,exploratory data analysis, building back and front end for machine learning application, continuous integration and development CI/CD of machine learning models using the following tools Python, JavaScript, SQL, Matlab, React, TensorFlow, Dockers, Flask, Heroku, Streamlit, AWS, GCP, Azure, Git, Spark, GitHub Actions, Keras, scikit‑learn, Google BigQuery,NextJS, 
+            With my expertise in building machine learning models,exploratory data analysis, building back and front end for machine learning applications, continuous integration and development CI/CD of machine learning models using the following tools Python, JavaScript, SQL, Matlab, React, TensorFlow, Dockers, Flask, Heroku, Streamlit, AWS, GCP, Azure, Git, Spark, GitHub Actions, Keras, scikit‑learn, Google BigQuery,NextJS, 
             Vscode,Vercel, Jenkins, Node.js, Fivetran, Gitlab, Jira, Kubeflow, CI/CD, Linux
             
           </p>
